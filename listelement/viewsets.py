ab615580312c8fffa8db1b585fdf3a1a4f1eabe4
Sandra Gutierrez <sandra2202gutierrez@gmail.com>
2020-06-18 20:48:43 -0500

    Crear metodos personalizados elementos dado categoria

--- listelement/viewsets.py
+++ listelement/viewsets.py
@@ -1,7 +1,12 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from django.shortcuts import get_object_or_404
 
 from .models import Element, Category, Type
 from .serializer import ElementSerializer, CategorySerializer, TypeSerializer
+
 
 class ElementViewSet(viewsets.ModelViewSet):
     queryset = Element.objects.all()
@@ -10,6 +15,12 @@ class ElementViewSet(viewsets.ModelViewSet):
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+    @action(detail=True, methods=['get'])
+    def elemets(self, request, pk=None):
+        queryset = Element.objects.filter(category_id=pk)
+        serializer = ElementSerializer(queryset, many=True)
+        return Response(serializer.data)
 
 class TypeViewSet(viewsets.ModelViewSet):
     queryset = Type.objects.all()
